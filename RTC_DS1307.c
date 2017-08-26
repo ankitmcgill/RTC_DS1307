2571a30266b17310a57551ec422b33e51a0245ff
Ankit Bhatnagar <ankit.bhatnagar@semaconnect.com>
2017-08-26 14:36:15 +0530

    finished. tested working

--- RTC_DS1307.c
+++ RTC_DS1307.c
@@ -55,9 +55,206 @@ void PUTINFLASH RTC_DS1307_SetI2CFunctions(void (*i2c_init)(uint8_t),
     }
 }
 
+uint8_t PUTINFLASH RTC_DS1307_GetControlRegister(void)
+{
+    //DS1307 RETURN CONTROL REGISTER VALUE
+
+    return (*_rtc_ds1307_i2c_readbyte)(RTC_DS1307_REGISTER_CONTROL);
+}
+
+RTC_DS1307_TIME*  PUTINFLASH RTC_DS1307_GetTime(void)
+{
+    //DS1307 GET CURRENT TIME
+
+    RTC_DS1307_TIME* time = (RTC_DS1307_TIME*)os_zalloc(sizeof(RTC_DS1307_TIME));
+
+    uint8_t byte = (*_rtc_ds1307_i2c_readbyte)(RTC_DS1307_REGISTER_SECONDS);
+    time->second = (RTC_DS1307_SECONDS_GET_DIG1(byte) * 10) + RTC_DS1307_SECONDS_GET_DIG0(byte);
+
+    byte = (*_rtc_ds1307_i2c_readbyte)(RTC_DS1307_REGISTER_MINUTES);
+    time->minute = (RTC_DS1307_MINUTES_GET_DIG1(byte) * 10) + RTC_DS1307_MINUTES_GET_DIG0(byte);
+
+    byte = (*_rtc_ds1307_i2c_readbyte)(RTC_DS1307_REGISTER_HOURS);
+    time->hour_format = RTC_DS1307_HOURS_GET_1224_BIT(byte);
+    if(time->hour_format == timeformat_12)
+    {
+        //12 HR FORMAT
+        time->am_pm = RTC_DS1307_HOURS_GET_AMPM_BIT(byte);
+        time->hour = (RTC_DS1307_HOURS_12_GET_DIG1(byte) * 10) + RTC_DS1307_HOURS_12_GET_DIG0(byte);
+    }
+    else
+    {
+          time->hour = (RTC_DS1307_HOURS_24_GET_DIG1(byte) * 10) + RTC_DS1307_HOURS_24_GET_DIG0(byte);
+    }
+
+    byte = (*_rtc_ds1307_i2c_readbyte)(RTC_DS1307_REGISTER_DAY);
+    time->day = RTC_DS1307_DAY_GET(byte);
+
+    byte = (*_rtc_ds1307_i2c_readbyte)(RTC_DS1307_REGISTER_DATE);
+    time->date = (RTC_DS1307_DATE_GET_DIG1(byte) * 10) + RTC_DS1307_DATE_GET_DIG0(byte);
+
+    byte = (*_rtc_ds1307_i2c_readbyte)(RTC_DS1307_REGISTER_MONTH);
+    time->month = (RTC_DS1307_MONTH_GET_DIG1(byte) * 10) + RTC_DS1307_MONTH_GET_DIG0(byte);
+
+    byte = (*_rtc_ds1307_i2c_readbyte)(RTC_DS1307_REGISTER_YEAR);
+    time->year = 2000 + (RTC_DS1307_YEAR_GET_DIG1(byte) * 10) + RTC_DS1307_YEAR_GET_DIG0(byte);
+
+    return time;
+}
+
+void PUTINFLASH RTC_DS1307_SetControlRegister(RTC_DS1307_CONTROL_CONFIG control)
+{
+    //DS1307 SET CONTROL REGISTER OPTIONS
+
+    uint8_t byte = 0;
+    if(control.out)
+    {
+        byte = RTC_DS1307_CONTROL_SET_OUT(byte);
+    }
+
+    if(control.sqwe)
+    {
+        byte = RTC_DS1307_CONTROL_SET_SQWE(byte);
+        byte = RTC_DS1307_CONTROL_SET_RS(byte, control.freq);
+    }
+
+    (*_rtc_ds1307_i2c_writebyte)(RTC_DS1307_REGISTER_CONTROL, byte);
+
+    if(_rtc_ds1307_debug)
+    {
+        PRINTF("RTC : DS1307 : Control register written with %x\n", byte);
+    }
+}
+
+void PUTINFLASH RTC_DS1307_SetTimeFormat(RTC_DS1307_TIMEFORMAT_CONFIG format)
+{
+    //SET DS1307 TIMEFORMAT (12/24 HRS)
+
+    uint8_t byte = 0;
+    if(format == timeformat_12)
+    {
+        //12 HRS
+        byte = RTC_DS1307_HOURS_SET12(byte);
+        if(_rtc_ds1307_debug)
+        {
+            PRINTF("RTC : DS1307 : Time format set to 12 hrs. byte = %x\n", byte);
+        }
+    }
+    else
+    {
+        //24 HRS
+        RTC_DS1307_HOURS_SET24(byte);
+        if(_rtc_ds1307_debug)
+        {
+            PRINTF("RTC : DS1307 : Time format set to 24 hrs. byte = %x\n", byte);
+        }
+    }
+    (*_rtc_ds1307_i2c_writebyte)(RTC_DS1307_REGISTER_HOURS, byte);
+}
+
+void PUTINFLASH RTC_DS1307_SetTime(RTC_DS1307_TIME time)
+{
+    //SET THE SPECIFIED TIME
+
+    //SECOND
+    uint8_t byte = 0;
+    byte = RTC_DS1307_SECONDS_SET_DIG1(byte, (time.second/10)) | RTC_DS1307_SECONDS_SET_DIG0(byte, (time.second % 10));
+    (*_rtc_ds1307_i2c_writebyte)(RTC_DS1307_REGISTER_SECONDS, byte);
+
+    //MINUTE
+    byte = 0;
+    byte = RTC_DS1307_MINUTES_SET_DIG1(byte, (time.minute/10)) | RTC_DS1307_MINUTES_SET_DIG0(byte, (time.minute % 10));
+    (*_rtc_ds1307_i2c_writebyte)(RTC_DS1307_REGISTER_MINUTES, byte);
+
+    //HOUR
+    byte = 0;
+    if(time.hour_format == timeformat_24)
+    {
+        //24 HR FORMAT
+        byte = RTC_DS1307_HOURS_SET24(byte);
+        byte = RTC_DS1307_HOURS_SET_DIG1(byte, (time.hour/10)) | RTC_DS1307_HOURS_SET_DIG0(byte, (time.hour % 10));
+    }
+    else
+    {
+        //12 HR FORMAT
+        byte = RTC_DS1307_HOURS_SET12(byte);
+        if(time.am_pm == timeformat_am)
+            byte = RTC_DS1307_HOURS_SETAM(byte);
+        else
+            byte = RTC_DS1307_HOURS_SETPM(byte);
+            byte = RTC_DS1307_HOURS_SET_DIG1(byte, (time.hour/10)) | RTC_DS1307_HOURS_SET_DIG0(byte, (time.hour % 10));
+    }
+    (*_rtc_ds1307_i2c_writebyte)(RTC_DS1307_REGISTER_HOURS, byte);
+
+    //DAY
+    byte = 0;
+    byte = RTC_DS1307_DAY_SET(byte, time.day);
+    (*_rtc_ds1307_i2c_writebyte)(RTC_DS1307_REGISTER_DAY, byte);
+
+    //DATE
+    byte = 0;
+    byte = RTC_DS1307_DATE_SET_DIG1(byte, (time.date/10)) | RTC_DS1307_DATE_SET_DIG0(byte, (time.date % 10));
+    (*_rtc_ds1307_i2c_writebyte)(RTC_DS1307_REGISTER_DATE, byte);
+
+    //MONTH
+    byte = 0;
+    byte = RTC_DS1307_MONTH_SET_DIG1(byte, (time.month/10)) | RTC_DS1307_MONTH_SET_DIG0(byte, (time.month % 10));
+    (*_rtc_ds1307_i2c_writebyte)(RTC_DS1307_REGISTER_MONTH, byte);
+
+    //YEAR
+    byte = 0;
+    byte = RTC_DS1307_YEAR_SET_DIG1(byte, ((time.year - 2000)/10)) | RTC_DS1307_YEAR_SET_DIG0(byte, ((time.year - 2000) % 10));
+    (*_rtc_ds1307_i2c_writebyte)(RTC_DS1307_REGISTER_YEAR, byte);
+
+    if(_rtc_ds1307_debug)
+    {
+        PRINTF("RTC : DS1307 : Time set to user specified !\n");
+    }
+}
+
 void PUTINFLASH RTC_DS1307_Initialize(void)
 {
     //INTIALIZE DS1307 RTC
+    //CHECK FOR CH BIT, IF DISABLED, ENABLE IT
 
+    //INIT BACKEND I2C
+    #if defined(ESP8266)
+        ESP8266_I2C_SetDebug(_rtc_ds1307_debug);
+        ESP8266_I2C_Init(RTC_DS1307_I2C_ADDRESS);
+    #endif
 
+    if(_rtc_ds1307_debug)
+    {
+        PRINTF("RTC : DS1307 : Initialized\n");
+    }
+}
+
+uint8_t PUTINFLASH _rtc_ds1307_verify_ch_bit(void)
+{
+    //CHECK THE CH BIT AND TURN ON OSCILLATOR IF OFF
+
+    //READ CH BIT
+    uint8_t byte = (*_rtc_ds1307_i2c_readbyte)(RTC_DS1307_REGISTER_SECONDS);
+
+    if(RTC_DS1307_SECONDS_GET_CH_BIT(byte))
+    {
+      if(_rtc_ds1307_debug)
+      {
+          PRINTF("RTC : DS1307 : CH bit disabled. Enabling it\n");
+      }
+
+      byte = 0;
+      (*_rtc_ds1307_i2c_writebyte)(RTC_DS1307_REGISTER_SECONDS, byte);
+
+      //CONFIRM CH WRITTEN
+      byte = (*_rtc_ds1307_i2c_readbyte)(RTC_DS1307_REGISTER_SECONDS);
+
+      if(RTC_DS1307_SECONDS_GET_CH_BIT(byte))
+      {
+        if(_rtc_ds1307_debug)
+        {
+            PRINTF("RTC : DS1307 : Error writing CH bit\n");
+        }
+      }
+    }
 }
--- RTC_DS1307.h
+++ RTC_DS1307.h
@@ -24,12 +24,13 @@
   #include "ip_addr.h"
   #include "espconn.h"
   #include "os_type.h"
+  #include "mem.h"
 
   #define PRINTF      os_printf
   #define PUTINFLASH  ICACHE_FLASH_ATTR
 #endif
 
-#define RTC_DS1307_I2C_ADDRESS        0xD0
+#define RTC_DS1307_I2C_ADDRESS        0x68
 #define RTC_DS1307_REGISTER_NVRAM_LEN 56
 
 //DS1307 REGISTER MAP
@@ -53,7 +54,7 @@
 
 //DS1307 TIME REGISTER OPERATIONS
 #define RTC_DS1307_SECONDS_GET_CH_BIT(x)    ((x & 0x80) >> 7)
-#define RTC_DS1307_SECONDS_SET_CH_BIT(x)    (x | 0x70)
+#define RTC_DS1307_SECONDS_SET_CH_BIT(x)    (x | 0x80)
 #define RTC_DS1307_SECONDS_GET_DIG0(x)      (x & 0x0F)
 #define RTC_DS1307_SECONDS_GET_DIG1(x)      ((x & 0x70) >> 4)
 #define RTC_DS1307_SECONDS_SET_DIG0(x,y)    (x | y)
@@ -61,10 +62,10 @@
 
 #define RTC_DS1307_MINUTES_GET_DIG0(x)      (x & 0x0F)
 #define RTC_DS1307_MINUTES_GET_DIG1(x)      ((x & 0xF0) >> 4)
-#define RTC_DS1307_MINUTES_SET_DIG0(x)      (x | y)
-#define RTC_DS1307_MINUTES_SET_DIG1(x)      (x | (y << 4))
+#define RTC_DS1307_MINUTES_SET_DIG0(x,y)      (x | y)
+#define RTC_DS1307_MINUTES_SET_DIG1(x,y)      (x | (y << 4))
 
-#define RTC_DS1307_HOURS_GET_1224_BIT(x)    ((x | 0x40) >> 6)
+#define RTC_DS1307_HOURS_GET_1224_BIT(x)    ((x & 0x40) >> 6)
 #define RTC_DS1307_HOURS_SET12(x)           (x | 0x40)
 #define RTC_DS1307_HOURS_SET24(x)           (x & ~(1 << 6))
 #define RTC_DS1307_HOURS_GET_AMPM_BIT(x)    ((x & 0x20) >> 5)
@@ -75,7 +76,7 @@
 #define RTC_DS1307_HOURS_24_GET_DIG0(x)     (x & 0x0F)
 #define RTC_DS1307_HOURS_24_GET_DIG1(x)     ((x & 0x30) >> 4)
 #define RTC_DS1307_HOURS_SET_DIG0(x, y)     (x | y)
-#define RTC_DS1307_HOURS_SET_DIG1(x, y)     (x | (y << 4)
+#define RTC_DS1307_HOURS_SET_DIG1(x, y)     (x | (y << 4))
 
 #define RTC_DS1307_DAY_GET(x)               (x & 0x07)
 #define RTC_DS1307_DAY_SET(x,y)             (x | y)
@@ -83,7 +84,7 @@
 #define RTC_DS1307_DATE_GET_DIG0(x)         (x & 0x0F)
 #define RTC_DS1307_DATE_GET_DIG1(x)         ((x & 0x30) >> 4)
 #define RTC_DS1307_DATE_SET_DIG0(x,y)       (x | y)
-#define RTC_DS1307_DATE_SET_DIG1(x,y)       (x | (y >> 4))
+#define RTC_DS1307_DATE_SET_DIG1(x,y)       (x | (y << 4))
 
 #define RTC_DS1307_MONTH_GET_DIG0(x)        (x & 0x0F)
 #define RTC_DS1307_MONTH_GET_DIG1(x)        ((x & 0x10) >> 4)
@@ -95,14 +96,12 @@
 #define RTC_DS1307_YEAR_SET_DIG0(x,y)       (x | y)
 #define RTC_DS1307_YEAR_SET_DIG1(x,y)       (x | (y << 4))
 
-#define RTC_DS1307_CONTROL_GET_OUT(x)       ((x & 0x70) >> 7)
-#define RTC_DS1307_CONTROL_SET_OUT(x)       (x | 0x70)
+#define RTC_DS1307_CONTROL_GET_OUT(x)       ((x & 0x80) >> 7)
+#define RTC_DS1307_CONTROL_SET_OUT(x)       (x | 0x80)
 #define RTC_DS1307_CONTROL_GET_SQWE(x)      ((x & 0x10) >> 4)
 #define RTC_DS1307_CONTROL_SET_SQWE(x)      (x | 0x10)
-#define RTC_DS1307_CONTROL_GET_RS1(x)       ((x & 0x02) >> 1)
-#define RTC_DS1307_CONTROL_SET_RS1(x)       (x | 0x02)
-#define RTC_DS1307_CONTROL_GET_RS0(x)       (x & 0x01)
-#define RTC_DS1307_CONTROL_SET_RS0(x)       (x | 0x01)
+#define RTC_DS1307_CONTROL_GET_RS(x)        (x & 0x03)
+#define RTC_DS1307_CONTROL_SET_RS(x,y)      (x | y)
 
 //CUSTOM VARIABLE STRUCTURES/////////////////////////////
 typedef enum
@@ -118,11 +117,42 @@ typedef struct
     uint8_t out;
     uint8_t sqwe;
     RTC_DS1307_SQW_FREQ freq;
-} RTC_DS1307_CONTROL;
+} RTC_DS1307_CONTROL_CONFIG;
+
+typedef enum
+{
+    timeformat_24 = 0,
+    timeformat_12
+} RTC_DS1307_TIMEFORMAT_CONFIG;
+
+typedef enum
+{
+    timeformat_am = 0,
+    timeformat_pm
+} RTC_DS1307_TIMEFORMAT_AMPM;
+
+typedef enum
+{
+    sunday = 0,
+    monday,
+    tuesday,
+    wednesday,
+    thrursday,
+    friday,
+    saturday
+}RTC_DS1307_DAYFORMAT_CONFIG;
 
 typedef struct
 {
-
+  uint8_t second;
+  uint8_t minute;
+  uint8_t hour;
+  RTC_DS1307_TIMEFORMAT_CONFIG hour_format;
+  RTC_DS1307_TIMEFORMAT_AMPM am_pm;
+  RTC_DS1307_DAYFORMAT_CONFIG day;
+  uint8_t date;
+  uint8_t month;
+  uint16_t year;
 } RTC_DS1307_TIME;
 //END CUSTOM VARIABLE STRUCTURES/////////////////////////
 
@@ -139,10 +169,12 @@ void PUTINFLASH RTC_DS1307_SetI2CFunctions(void (*i2c_init)(uint8_t),
 
 //GET PARAMETERS FUNCTIONS
 uint8_t PUTINFLASH RTC_DS1307_GetControlRegister(void);
-void PUTINFLASH RTC_DS1307_GetTime(void);
+RTC_DS1307_TIME*  PUTINFLASH RTC_DS1307_GetTime(void);
 
 //SET PARAMETERS FUNCTIONS
-void PUTINFLASH RTC_DS1307_SetControlRegister(RTC_DS1307_CONTROL control);
+void PUTINFLASH RTC_DS1307_SetControlRegister(RTC_DS1307_CONTROL_CONFIG control);
+void PUTINFLASH RTC_DS1307_SetTimeFormat(RTC_DS1307_TIMEFORMAT_CONFIG format);
+void PUTINFLASH RTC_DS1307_SetTime(RTC_DS1307_TIME time);
 
 //CONTROL FUNCTIONS
 void PUTINFLASH RTC_DS1307_StartOcillator(void);
@@ -150,6 +182,6 @@ void PUTINFLASH RTC_DS1307_StopOscillator(void);
 void PUTINFLASH RTC_DS1307_Initialize(void);
 
 //INTERNAL FUNCTIONS
-uint8_t PUTINFLASH _rtc_ds1307_get_ch_bit(void);
+uint8_t PUTINFLASH _rtc_ds1307_verify_ch_bit(void);
 //END FUNCTION PROTOTYPES/////////////////////////////////
 #endif
